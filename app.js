05cb91b6af968c6e4ce704c4156f3bc1dab00820
Daniel <danieltapp@gmail.com>
2017-11-16 15:39:58 -0500

    Updated quotes and CSS

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var quotes = [
 'Kemba Walker for three..got it! WHOOPTIE DOOOOOOOO!!!!',
 'Batum goes the dynamite!',
-'Jeremy Lamb with the little DIPSY DOOOOO!'
+'Jeremy Lamb with the little DIPSY DOOOOO!',
+'Lordy, Lordy, Here Comes 40! Cody Zeller with the SLAM!',
+'Malik Monk! How do you dooooooo?!?!',
+'MKG with the gorgeous, pull up jumper! He has been an assasin from the mid-range!',
+'Dwayne Bacon! Oh my! Wakey, Wakey! And-1 for Bakey!'
+
 ]
 
  
