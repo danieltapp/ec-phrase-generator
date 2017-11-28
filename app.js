5f40efd532a6630fb13d256a05c15e22f661e03a
Daniel <danieltapp@gmail.com>
2017-11-28 14:30:35 -0500

    Updated JS so that it now concatenates a player name and phrase together to create a quote (instead of just querying a random quote). Also updated stylesheet to utilize bootstrap grid system and buttons.

--- app.js
+++ app.js
@@ -1,17 +1,79 @@
-var quotes = [
-'Kemba Walker for three..got it! WHOOPTIE DOOOOOOOO!!!!',
-'Batum goes the dynamite!',
-'Jeremy Lamb with the little DIPSY DOOOOO!',
-'Lordy, Lordy, Here Comes 40! Cody Zeller with the SLAM!',
-'Malik Monk! How do you dooooooo?!?!',
-'MKG with the gorgeous, pull up jumper! He has been an assasin from the mid-range!',
-'Dwayne Bacon! Oh my! Wakey, Wakey! And-1 for Bakey!'
+var players = [
+'Dwayne Bacon',
+'Nicolas Batum',
+'Michael Carter-Williams',
+'Treveon Graham',
+'Dwight Howard',
+'Frank Kaminsky',
+'Michael Kidd-Gilchrist',
+'Jeremy Lamb',
+'Mangok Mathiang',
+'Malik Monk',
+"Johnny O'Bryant",
+'Marcus Paige',
+'Julyan Stone',
+'Kemba Walker',
+'Marvin Williams',
+'Cody Zeller',
+'Kemba',
+'MKG',
+'Cody',
+'Nic',
+'The Frenchman',
+'Frank the Tank',
+'Monk',
+'Bacon',
+'Malik',
+'Cody Zeller! The Z Man',
+'Special K! Oh my Kaminsky'
+]
 
+var quotes = [
+' for three..got it! WHOOPTIE DOOOOOOOO!!!!',
+', jiminy crickets that was incredible!',
+' with the little DIPSY DOOOOO!',
+'  with the SLAM!',
+'! How do you dooooooo?!?!',
+' with the gorgeous, pull up jumper! He has been an assasin from the mid-range!',
+' is tougher than a two dollar steak!',
+' with a pair of beauties from the charity stripe.',
+' with the marvelous 3!',
+' with the guts of a cat burglar!',
+' takes it right to Goliath and scores!',
+', with the wingspan of a condor!',
+' is the truth!',
+', please sir can I have some more!',
+' is putting on a clinic tonight!',
+", we're not worthy!!!!",
+"! by the beard of zeus!?!?!",
+'! Are you kidding me? What splendor!',
+'!!! He who pays the piper calls the tune!',
+"! Put on a raincoast, because he's making it rain in here tonight!",
+'!!! Are you kidding me?!',
+', how great thou art!',
+' can do no wrong!',
+'!!! (Wilhelm scream sound effect)',
+'! A man among men!',
+'! Sweet Christmas!',
+'!!! One small step for man, one giant leap for mankind!!!',
+'! Hallowed be thy game!',
+'! My heart just skipped a beat!',
+" is doing the lord's work this evening!!!",
+'!!! Goodie goodie gum drops!!!!!',
+' is a gentlemen and a scholar.',
+'! someone call 9-11, because this man is on fire!!!',
+' is seeing an extra big basket tonight',
+' has the magic touch. He could teach David Copperfield a thing or two.',
+" is getting a hero's welcome.",
+'!!!!!!!!!!!!!!!!!!!!!!!!!!',
+'!!! Dunk, dunk dooce!',
+". It's just not fair what this man is capable of."
 ]
 
  
 function newQuote(){
-	var randomNumber = Math.floor(Math.random() * (quotes.length));
-	document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+	var name = Math.floor(Math.random() * (players.length));
+	var phrase = Math.floor(Math.random() * (quotes.length));
+	document.getElementById('quoteDisplay').innerHTML = players[name] + quotes[phrase];
 };
 
