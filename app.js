c0b8e4b8a3b714bd166dd036d32f16a7452074f1
Daniel <danieltapp@gmail.com>
2018-07-30 08:43:39 -0400

    Added functionality for Twitter button

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var players = [
+const players = [
 'Dwayne Bacon',
 'Nicolas Batum',
 'Michael Carter-Williams',
@@ -28,52 +28,68 @@ var players = [
 'Special K! Oh my Kaminsky'
 ]
 
-var quotes = [
-' for three..got it! WHOOPTIE DOOOOOOOO!!!!',
-', jiminy crickets that was incredible!',
-' with the little DIPSY DOOOOO!',
-'  with the SLAM!',
-'! How do you dooooooo?!?!',
-' with the gorgeous, pull up jumper! He has been an assassin from the mid-range!',
-' is tougher than a two dollar steak!',
-' with a pair of beauties from the charity stripe.',
-' with the marvelous 3!',
-' with the guts of a cat burglar!',
-' takes it right to Goliath and scores!',
-', with the wingspan of a condor!',
-' is the truth!',
-', please sir can I have some more?!',
-' is putting on a clinic tonight!',
-", we're not worthy!!!!",
-"! by the beard of zeus!?!?!",
-'! Are you kidding me? What splendor!',
-'!!! He who pays the piper calls the tune!',
-"! Put on a raincoat, because he's making it rain in here tonight!",
-'!!! Are you kidding me?!',
-', how great thou art!',
-' can do no wrong!',
-'!!! (Wilhelm scream sound effect)',
-'! A man among men!',
-'! Sweet Christmas!',
-'!!! One small step for man, one giant leap for mankind!!!',
-'! Hallowed be thy game!',
-'! My heart just skipped a beat!',
-" is doing the lord's work this evening!!!",
-'!!! Goodie goodie gum drops!!!!!',
-' is a gentlemen and a scholar.',
-'! someone call 9-11, because this man is on fire!!!',
-' is seeing an extra big basket tonight',
-' has the magic touch. He could teach David Copperfield a thing or two.',
-" is getting a hero's welcome.",
-'!!!!!!!!!!!!!!!!!!!!!!!!!!',
-'!!! Dunk, dunk deuce!',
-". It's just not fair what this man is capable of."
-]
+// var quotes = [
+// ' for three..got it! WHOOPTIE DOOOOOOOO!!!!',
+// ', jiminy crickets that was incredible!',
+// ' with the little DIPSY DOOOOO!',
+// '  with the SLAM!',
+// '! How do you dooooooo?!?!',
+// ' with the gorgeous, pull up jumper! He has been an assassin from the mid-range!',
+// ' is tougher than a two dollar steak!',
+// ' with a pair of beauties from the charity stripe.',
+// ' with the marvelous 3!',
+// ' with the guts of a cat burglar!',
+// ' takes it right to Goliath and scores!',
+// ', with the wingspan of a condor!',
+// ' is the truth!',
+// ', please sir can I have some more?!',
+// ' is putting on a clinic tonight!',
+// ", we're not worthy!!!!",
+// "! by the beard of zeus!?!?!",
+// '! Are you kidding me? What splendor!',
+// '!!! He who pays the piper calls the tune!',
+// "! Put on a raincoat, because he's making it rain in here tonight!",
+// '!!! Are you kidding me?!',
+// ', how great thou art!',
+// ' can do no wrong!',
+// '!!! (Wilhelm scream sound effect)',
+// '! A man among men!',
+// '! Sweet Christmas!',
+// '!!! One small step for man, one giant leap for mankind!!!',
+// '! Hallowed be thy game!',
+// '! My heart just skipped a beat!',
+// " is doing the lord's work this evening!!!",
+// '!!! Goodie goodie gum drops!!!!!',
+// ' is a gentlemen and a scholar.',
+// '! someone call 9-11, because this man is on fire!!!',
+// ' is seeing an extra big basket tonight',
+// ' has the magic touch. He could teach David Copperfield a thing or two.',
+// " is getting a hero's welcome.",
+// '!!!!!!!!!!!!!!!!!!!!!!!!!!',
+// '!!! Dunk, dunk deuce!',
+// ". It's just not fair what this man is capable of."
+// ]
+
+const madeBy =
+	" - Made by The Eric Collins Random Quote Generator, https://codepen.io/danieltapp/full/eyQPML/";
+
+const quoteUrl = 'https://quotes.djones.co/api/collection/eric-collins-quotes/random';
 
  
-function newQuote(){
-	var name = Math.floor(Math.random() * (players.length));
-	var phrase = Math.floor(Math.random() * (quotes.length));
-	document.getElementById('quoteDisplay').innerHTML = players[name] + quotes[phrase];
-};
+function newQuote() {
+	$.getJSON(quoteUrl, function (json) {
+		var phrase = json.content;
+		var name = Math.floor(Math.random() * players.length);
+		document.getElementById("quoteDisplay").innerHTML =
+			"'" + players[name] + phrase + "'";
+	});
+}
 
+$(".twitter-share-button").click(function () {
+		$(this).attr(
+			"href",
+			"https://twitter.com/intent/tweet?text=" +
+			document.getElementById("quoteDisplay").innerHTML +
+			madeBy
+		);
+	});
